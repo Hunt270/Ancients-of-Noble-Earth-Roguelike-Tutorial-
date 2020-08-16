3d554fe78997963a1bd6d732df0064488cba6035
Will <37303629+Hunt270@users.noreply.github.com>
2020-08-16 16:39:25 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def main() -> None:
 		screen_width,
 		screen_height,
 		tileset=tileset,
-		title="Yet Another Roguelike Tutorial",
+		title="Ancients of Noble Earth",
 		vsync=True,
 	) as context:
 			root_console = tcod.Console(screen_width, screen_height, order="F")
